something is wrong with SMA calculation but time is up

back_testing_center.py
@@ -3,6 +3,8 @@ import numpy as np
 from utilities import Baseutils
 import random
 from typing import List, Tuple
+from matplotlib import pyplot as plt
+
 class Back_Test:
     def __init__(self, symbol):
         self.baseutils = Baseutils() #if you want to specify start and end date put them here.
@@ -10,8 +12,8 @@ class Back_Test:
         self.prices : pd.DataFrame = self.dataframe["Close"]
     
     def make_returns(self) -> pd.DataFrame:
-        prices :pd.DataFrame = self.prices
-        Returns : pd.DataFrame  = np.log(prices/prices.shift(1))
+        prices :pd.Series = self.prices
+        Returns : pd.Series  = np.log(prices/prices.shift(1))
         return Returns
 
     def make_random_positions(self) -> List[int]:
@@ -22,5 +24,16 @@ class Back_Test:
         for i in range(len(self.prices)):
             strategy.append(random.randint(-1,1))
         return np.array(strategy)
-            
-
+    
+    
+    def make_result(self,data:pd.DataFrame):
+        last = 100
+        data["profit"] = data["returns"].shift(1)*data["position"]
+        plt.plot(data["returns"][-last:])
+        plt.plot(data["profit"][-last:])
+        plt.show()
+        original_capital = 10000 
+        result_profit = original_capital * np.exp(np.sum(data["profit"]))
+        hold_profit = original_capital * np.exp(np.sum(data["returns"]))
+        print(int(result_profit-original_capital),"result_profit with strat")
+        print(int(hold_profit-original_capital),"theoretical profit when holding until the last day in data.")

tests.py
@@ -1,5 +1,6 @@
 from utilities import Baseutils
 from back_testing_center import Back_Test
+from SMA import SMA_related
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
@@ -11,6 +12,7 @@ class Test:
         result = []
         result.append(self.Baseutils_test())
         result.append(self.Back_Test_test())
+        result.append(self.SMA_related_test())
         if False in result:
             print("failed")
             return False
@@ -18,6 +20,15 @@ class Test:
             print("passed")
             return True 
 
+    def SMA_related_test(self):
+        try:
+            sma_related = SMA_related()
+            
+            sma_related.back_test_main("NEE")
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def Baseutils_test(self):
         baseutil = Baseutils()
@@ -31,7 +42,7 @@ class Test:
 
     def Back_Test_test(self):
         
-        backtest = Back_Test("^N225")
+        backtest = Back_Test("NEE")
         last = 30
         returns = backtest.make_returns()
         print(returns)
@@ -50,12 +61,14 @@ class Test:
         #ax = data.plot(secondary_y="Position",figsize=(10,6))
         #ax.get_legend().set_bbox_to_anchor((0.25,0.85))
         #ax.figure.show()
+        
         theoretical_latest = backtest.prices[0]*np.exp(np.sum(returns))
         the_latest = backtest.prices[-1]
         print(backtest.prices[0],"first price")
         print(theoretical_latest,"hold")
         print(the_latest,"the latest price")
         print(10000*np.exp(np.sum(profit)),"strat")
+        print(type(backtest.prices))
         if round(theoretical_latest,2) == round(the_latest,2):
             print("logged return test ok")
             return True